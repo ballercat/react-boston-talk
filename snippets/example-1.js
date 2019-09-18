fa4d7a704d306af0ef44d2f8d84edcb83ac47633
Arty Buldauskas <abuldauskas@wayfair.com>
2019-09-18 16:01:26 -0400

    Get a sort of decent talk going

--- snippets/example-1.js
+++ snippets/example-1.js
@@ -1,5 +1,8 @@
 test("function adds 4", () => {
   const original = [1, 2, 3, 4];
   const results = fn(original);
-  results.forEach((result, i) => expect(result).toBe(original[i] + 4));
+
+  results.forEach(function(result, i) {
+    expect(result).toBe(original[i] + 4);
+  });
 });
