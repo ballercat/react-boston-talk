3442d8c7aa38565b75f59dd4823047a9ca0efe57
Arty Buldauskas <abuldauskas@wayfair.com>
2019-09-21 11:53:28 -0400

    update

--- snippets/example-4.js
+++ snippets/example-4.js
@@ -2,5 +2,8 @@ test("hasAssertions should fail expects in promises", () => {
   expect.hasAssertions();
   // Note that the expect below does not "count" as a failed assertion for this
   // test, but hasAssertions() also does not fail as it should!
-  Promise.resolve().then(() => expect(true).toBe(false));
+  const promise = Promise.resolve();
+  promise.then(() => expect(true).toBe(false));
+
+  return promise;
 });
